f1003417e18e7d58dcc92fb364b7cee3ef1b4263
Mário Pereira <marioscpereira@gmail.com>
2022-01-10 18:37:14 +0000

    Displaying all records of DB on Dashboard, including real time saves, updates and deletes.

--- CSharpForm1/Dashboard.Designer.cs
+++ CSharpForm1/Dashboard.Designer.cs
@@ -259,6 +259,7 @@
             this.Controls.Add(this.panel1);
             this.Name = "Dashboard";
             this.Text = "Dashboard";
+            this.Load += new System.EventHandler(this.Dashboard_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvEmployee)).EndInit();
